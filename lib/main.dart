162ad3f51b60af9cd13ec2dbd69323e6a5931ed8
Thomas Clark <tnc1997@outlook.com>
2025-06-01 08:08:23 +0100

    refactor(welcome): add postcode form field

--- lib/main.dart
+++ lib/main.dart
@@ -173,6 +173,35 @@ class HistoricalChargeListTile extends StatelessWidget {
   }
 }
 
+class PostcodeFormField extends StatelessWidget {
+  const PostcodeFormField({
+    super.key,
+    required this.controller,
+  });
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return TextFormField(
+      controller: controller,
+      decoration: const InputDecoration(
+        label: Text('Postcode'),
+        border: OutlineInputBorder(),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your postcode.';
+        }
+
+        return null;
+      },
+    );
+  }
+}
+
 class OctopusEnergyApiClient {
   final http.Client _client;
 
