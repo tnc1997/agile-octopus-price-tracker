e57d1b566510896d8a590d760bcb1e058e6c365d
Thomas Clark <tnc1997@outlook.com>
2025-06-03 06:50:05 +0100

    refactor(welcome): add continue button

--- lib/main.dart
+++ lib/main.dart
@@ -148,6 +148,56 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+class ContinueButton extends StatelessWidget {
+  const ContinueButton({
+    super.key,
+    required this.formKey,
+    required this.importProductCodeNotifier,
+    required this.postcodeController,
+  });
+
+  final GlobalKey<FormState> formKey;
+
+  final ValueNotifier<String?> importProductCodeNotifier;
+
+  final TextEditingController postcodeController;
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: () async {
+        if (formKey.currentState case final state?) {
+          if (state.validate()) {
+            final client = context.read<OctopusEnergyApiClient>();
+            final preferences = context.read<SharedPreferencesAsync>();
+            final router = GoRouter.of(context);
+
+            final list = await client.industry.listIndustryGridSupplyPoints(
+              page: 1,
+              postcode: postcodeController.value.text,
+            );
+
+            await preferences.setString(
+              'import_product_code',
+              importProductCodeNotifier.value!,
+            );
+
+            await preferences.setString(
+              'region_code',
+              list.results.single.groupId,
+            );
+
+            router.go(
+              const HomeRoute().location,
+            );
+          }
+        }
+      },
+      child: const Text('Continue'),
+    );
+  }
+}
+
 class HistoricalChargeListTile extends StatelessWidget {
   const HistoricalChargeListTile({
     super.key,
