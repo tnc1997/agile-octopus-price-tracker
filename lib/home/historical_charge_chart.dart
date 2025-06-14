103773220f041958c78df801ba899e45c773a703
Thomas Clark <tnc1997@outlook.com>
2025-06-14 10:44:25 +0100

    feat: add point color mapper to chart series

--- lib/home/historical_charge_chart.dart
+++ lib/home/historical_charge_chart.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
+import '../common/num_extensions.dart';
 import '../main.dart';
 
 class HistoricalChargeChart extends StatelessWidget {
+  static const _stops = [
+    (Color(0xff00ff00), 10),
+    (Color(0xffffff00), 20),
+    (Color(0xffff0000), 30),
+  ];
+
   const HistoricalChargeChart({
     super.key,
     required this.historicalCharges,
@@ -41,6 +48,28 @@ class HistoricalChargeChart extends StatelessWidget {
           },
           sortFieldValueMapper: (datum, index) {
             return datum.validFrom!.toLocal();
+          },
+          pointColorMapper: (datum, index) {
+            for (var i = 0; i < _stops.length - 1; i++) {
+              if (datum.valueIncVat! < _stops[i].$2) {
+                return _stops[i].$1;
+              }
+
+              if (datum.valueIncVat! < _stops[i + 1].$2) {
+                return Color.lerp(
+                  _stops[i].$1,
+                  _stops[i + 1].$1,
+                  datum.valueIncVat!.remap(
+                    _stops[i].$2,
+                    _stops[i + 1].$2,
+                    0,
+                    1,
+                  ),
+                );
+              }
+            }
+
+            return _stops.last.$1;
           },
         ),
       ],
