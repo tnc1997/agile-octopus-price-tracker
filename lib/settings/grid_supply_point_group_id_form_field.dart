40f944ab660f63eb09c0c9b2f4bcf110d9605315
Thomas Clark <tnc1997@outlook.com>
2025-06-17 16:16:36 +0100

    feat(settings): add location icon button

--- lib/settings/grid_supply_point_group_id_form_field.dart
+++ lib/settings/grid_supply_point_group_id_form_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 
 import '../main.dart';
@@ -70,38 +71,157 @@ class _GridSupplyPointGroupIdFormFieldState
         ).toList();
 
         if (items != null) {
-          return ValueListenableBuilder(
-            valueListenable: widget.notifier,
-            builder: (context, value, child) {
-              return DropdownButtonFormField<String>(
-                items: items,
-                value: value,
-                onChanged: (value) {
-                  widget.notifier.value = value;
+          return Row(
+            spacing: 16.0,
+            children: [
+              Expanded(
+                child: ValueListenableBuilder(
+                  valueListenable: widget.notifier,
+                  builder: (context, value, child) {
+                    return DropdownButtonFormField<String>(
+                      items: items,
+                      value: value,
+                      onChanged: (value) {
+                        widget.notifier.value = value;
+                      },
+                      decoration: const InputDecoration(
+                        label: Text('Region'),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please select your region.';
+                        }
+
+                        return null;
+                      },
+                    );
+                  },
+                ),
+              ),
+              IconButton(
+                onPressed: () async {
+                  final client = context.read<OctopusEnergyApiClient>();
+                  final messenger = ScaffoldMessenger.of(context);
+                  final nominatim = context.read<NominatimApiClient>();
+
+                  if (!await Geolocator.isLocationServiceEnabled()) {
+                    messenger.showSnackBar(
+                      const SnackBar(
+                        content: Text('Please enable location services.'),
+                      ),
+                    );
+
+                    return;
+                  }
+
+                  var permission = await Geolocator.checkPermission();
+
+                  if (permission == LocationPermission.denied) {
+                    permission = await Geolocator.requestPermission();
+
+                    if (permission == LocationPermission.denied) {
+                      messenger.showSnackBar(
+                        const SnackBar(
+                          content: Text('Please allow location permissions.'),
+                        ),
+                      );
+
+                      return;
+                    }
+                  }
+
+                  if (permission == LocationPermission.deniedForever) {
+                    messenger.showSnackBar(
+                      const SnackBar(
+                        content: Text('Please allow location permissions.'),
+                      ),
+                    );
+
+                    return;
+                  }
+
+                  final Position position;
+
+                  try {
+                    position = await Geolocator.getCurrentPosition();
+                  } catch (e) {
+                    messenger.showSnackBar(
+                      const SnackBar(
+                        content: Text('Failed to get the location.'),
+                      ),
+                    );
+
+                    return;
+                  }
+
+                  final String postcode;
+
+                  try {
+                    final place = await nominatim.reverse(
+                      position.latitude,
+                      position.longitude,
+                    );
+
+                    postcode = place.address!.postcode!;
+                  } catch (e) {
+                    messenger.showSnackBar(
+                      const SnackBar(
+                        content: Text('Failed to get the postcode.'),
+                      ),
+                    );
+
+                    return;
+                  }
+
+                  final String groupId;
+
+                  try {
+                    final list =
+                        await client.industry.listIndustryGridSupplyPoints(
+                      page: 1,
+                      postcode: postcode,
+                    );
+
+                    groupId = list.results!.first.groupId!;
+                  } catch (e) {
+                    messenger.showSnackBar(
+                      const SnackBar(
+                        content: Text('Failed to get the group identifier.'),
+                      ),
+                    );
+
+                    return;
+                  }
+
+                  widget.notifier.value = groupId;
                 },
+                tooltip: 'Location',
+                icon: Icon(Icons.my_location),
+              ),
+            ],
+          );
+        }
+
+        return Row(
+          spacing: 16.0,
+          children: [
+            Expanded(
+              child: DropdownButtonFormField<String>(
+                items: const [],
+                onChanged: null,
                 decoration: const InputDecoration(
                   label: Text('Region'),
                   border: OutlineInputBorder(),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please select your region.';
-                  }
-
-                  return null;
-                },
-              );
-            },
-          );
-        }
-
-        return DropdownButtonFormField<String>(
-          items: const [],
-          onChanged: null,
-          decoration: const InputDecoration(
-            label: Text('Region'),
-            border: OutlineInputBorder(),
-          ),
+              ),
+            ),
+            IconButton(
+              onPressed: null,
+              tooltip: 'Location',
+              icon: Icon(Icons.my_location),
+            ),
+          ],
         );
       },
     );
