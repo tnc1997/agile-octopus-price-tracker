796d2e85dbbce8735bf60ff56d0699646e243ca9
Thomas Clark <tnc1997@outlook.com>
2025-06-17 16:09:03 +0100

    fix(settings): import product code notifier

--- lib/settings/import_product_code_form_field.dart
+++ lib/settings/import_product_code_form_field.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ImportProductCodeFormField extends StatelessWidget {
+  static const _items = [
+    DropdownMenuItem<String>(
+      value: 'AGILE-24-10-01',
+      child: Text('Agile Octopus October 2024 v1'),
+    ),
+    DropdownMenuItem<String>(
+      value: 'AGILE-24-04-03',
+      child: Text('Agile Octopus April 2024 v1'),
+    ),
+    DropdownMenuItem<String>(
+      value: 'AGILE-23-12-06',
+      child: Text('Agile Octopus December 2023 v1'),
+    ),
+    DropdownMenuItem<String>(
+      value: 'AGILE-FLEX-22-11-25',
+      child: Text('Agile Octopus November 2022 v1'),
+    ),
+    DropdownMenuItem<String>(
+      value: 'AGILE-22-08-31',
+      child: Text('Agile Octopus August 2022 v1'),
+    ),
+    DropdownMenuItem<String>(
+      value: 'AGILE-22-07-22',
+      child: Text('Agile Octopus July 2022 v1'),
+    ),
+    DropdownMenuItem<String>(
+      value: 'AGILE-18-02-21',
+      child: Text('Agile Octopus February 2018'),
+    ),
+  ];
+
   const ImportProductCodeFormField({
     super.key,
     required this.notifier,
@@ -12,51 +43,27 @@ class ImportProductCodeFormField extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
-    return DropdownButtonFormField<String>(
-      items: const [
-        DropdownMenuItem<String>(
-          value: 'AGILE-24-10-01',
-          child: Text('Agile Octopus October 2024 v1'),
-        ),
-        DropdownMenuItem<String>(
-          value: 'AGILE-24-04-03',
-          child: Text('Agile Octopus April 2024 v1'),
-        ),
-        DropdownMenuItem<String>(
-          value: 'AGILE-23-12-06',
-          child: Text('Agile Octopus December 2023 v1'),
-        ),
-        DropdownMenuItem<String>(
-          value: 'AGILE-FLEX-22-11-25',
-          child: Text('Agile Octopus November 2022 v1'),
-        ),
-        DropdownMenuItem<String>(
-          value: 'AGILE-22-08-31',
-          child: Text('Agile Octopus August 2022 v1'),
-        ),
-        DropdownMenuItem<String>(
-          value: 'AGILE-22-07-22',
-          child: Text('Agile Octopus July 2022 v1'),
-        ),
-        DropdownMenuItem<String>(
-          value: 'AGILE-18-02-21',
-          child: Text('Agile Octopus February 2018'),
-        ),
-      ],
-      value: notifier.value,
-      onChanged: (value) {
-        notifier.value = value;
-      },
-      decoration: const InputDecoration(
-        label: Text('Tariff'),
-        border: OutlineInputBorder(),
-      ),
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'Please select your tariff.';
-        }
+    return ValueListenableBuilder(
+      valueListenable: notifier,
+      builder: (context, value, child) {
+        return DropdownButtonFormField<String>(
+          items: _items,
+          value: value,
+          onChanged: (value) {
+            notifier.value = value;
+          },
+          decoration: const InputDecoration(
+            label: Text('Tariff'),
+            border: OutlineInputBorder(),
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please select your tariff.';
+            }
 
-        return null;
+            return null;
+          },
+        );
       },
     );
   }
