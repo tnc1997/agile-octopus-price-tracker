b9230f6357708dd974e55d48803264ede4bd06fe
Thomas Clark <tnc1997@outlook.com>
2025-06-23 08:08:08 +0100

    refactor(welcome): add card widgets to welcome screen

--- lib/welcome/welcome_screen.dart
+++ lib/welcome/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../settings/tariff_form.dart';
+import '../settings/tariff_form_card.dart';
+import 'welcome_card.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({
@@ -13,8 +14,19 @@ class WelcomeScreen extends StatelessWidget {
   ) {
     return const Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: TariffForm(),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              spacing: 16.0,
+              children: [
+                WelcomeCard(),
+                TariffFormCard(),
+              ],
+            ),
+          ),
         ),
       ),
     );
