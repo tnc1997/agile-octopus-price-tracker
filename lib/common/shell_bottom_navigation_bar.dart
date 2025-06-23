06fa932aa5d9072f88c64b30645f70f1e47d90a0
Thomas Clark <tnc1997@outlook.com>
2025-06-23 08:10:30 +0100

    refactor(common): extract shell bottom navigation bar widget

---
+++ lib/common/shell_bottom_navigation_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../home/home_route.dart';
+import '../settings/settings_route.dart';
+import 'shell_route.dart';
+
+class ShellBottomNavigationBar extends StatelessWidget {
+  const ShellBottomNavigationBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],
+      onTap: (index) {
+        switch (index) {
+          case 0:
+            const HomeRoute().go(context);
+          case 1:
+            const SettingsRoute().go(context);
+          default:
+            const HomeRoute().go(context);
+        }
+      },
+      currentIndex: switch (GoRouterState.of(context).uri.path) {
+        '/' => 0,
+        '/settings' => 1,
+        _ => 0,
+      },
+    );
+  }
+}
--- lib/common/shell_screen.dart
+++ lib/common/shell_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
-import '../home/home_route.dart';
-import '../settings/settings_route.dart';
-import 'shell_route.dart';
+import 'shell_bottom_navigation_bar.dart';
 
 class ShellScreen extends StatelessWidget {
   const ShellScreen({
@@ -21,33 +18,7 @@ class ShellScreen extends StatelessWidget {
       body: SafeArea(
         child: child,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              const HomeRoute().go(context);
-            case 1:
-              const SettingsRoute().go(context);
-            default:
-              const HomeRoute().go(context);
-          }
-        },
-        currentIndex: switch (GoRouterState.of(context).uri.path) {
-          '/' => 0,
-          '/settings' => 1,
-          _ => 0,
-        },
-      ),
+      bottomNavigationBar: const ShellBottomNavigationBar(),
     );
   }
 }
