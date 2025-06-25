0df5d6c90fb873811bcc92c7de5cc99c7d96d7d6
Thomas Clark <tnc1997@outlook.com>
2025-06-25 19:38:14 +0100

    refactor(common): add shell navigation rail widget

---
+++ lib/common/shell_navigation_rail.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../home/home_route.dart';
+import '../settings/settings_route.dart';
+import 'shell_route.dart';
+
+class ShellNavigationRail extends StatelessWidget {
+  const ShellNavigationRail({
+    super.key,
+  });
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return NavigationRail(
+      destinations: const [
+        NavigationRailDestination(
+          icon: Icon(Icons.home),
+          label: Text('Home'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.settings),
+          label: Text('Settings'),
+        ),
+      ],
+      selectedIndex: switch (GoRouterState.of(context).uri.path) {
+        '/' => 0,
+        '/settings' => 1,
+        _ => 0,
+      },
+      onDestinationSelected: (index) {
+        switch (index) {
+          case 0:
+            const HomeRoute().go(context);
+          case 1:
+            const SettingsRoute().go(context);
+          default:
+            const HomeRoute().go(context);
+        }
+      },
+      labelType: NavigationRailLabelType.all,
+    );
+  }
+}
