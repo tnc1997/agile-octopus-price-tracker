165461a2e80f7b9803d0043cfb00bf500cd7a2d5
Thomas Clark <tnc1997@outlook.com>
2025-06-26 19:47:53 +0100

    feat(common): add responsive navigation to shell screen

--- lib/common/shell_screen.dart
+++ lib/common/shell_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'shell_bottom_navigation_bar.dart';
+import 'shell_navigation_rail.dart';
 
 class ShellScreen extends StatelessWidget {
   const ShellScreen({
@@ -14,11 +15,31 @@ class ShellScreen extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
-    return Scaffold(
-      body: SafeArea(
-        child: child,
-      ),
-      bottomNavigationBar: const ShellBottomNavigationBar(),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 992) {
+          return Scaffold(
+            body: SafeArea(
+              child: Row(
+                children: [
+                  const ShellNavigationRail(),
+                  const VerticalDivider(),
+                  Expanded(
+                    child: child,
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return Scaffold(
+            body: SafeArea(
+              child: child,
+            ),
+            bottomNavigationBar: const ShellBottomNavigationBar(),
+          );
+        }
+      },
     );
   }
 }
